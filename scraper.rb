56b8938991b6e8a1757a5ce67ff867acc11c66ca
Max Wofford <max.speed.wofford@gmail.com>
2014-10-19 02:46:20 -0600

    cleaned up scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -14,9 +14,6 @@ output << '['
 (1..365).each do |pg|
     url = base_url + url_addition
     Nokogiri::HTML(open(url)).css("a.thumb").each do |box|
-        # binding.pry
-        # puts box
-        # puts "--------------------------------------------------"
         link = box["href"]
         category = box["title"]
         output << "{\"catagory\"   : \"#{category}\",
@@ -25,7 +22,7 @@ output << '['
     end
     puts "I just scraped Daily Deviations from #{date}!"
     sleep 1.0 + rand # sleep to avoid hitting the server too frequently
-    date -= 1
+    date -= 1 # move down one day
 end
 
 output << "]"
